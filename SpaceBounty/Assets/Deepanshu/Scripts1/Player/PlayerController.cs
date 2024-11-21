a23d3a026d79f6f5525fd9f006d5ea2c65963468
deepanshu <deepanshu120100@gmail.com>
2024-11-20 21:01:49 -0700

    Update1v
    +Added thrust, roll, yaw
    +Added particle for star effect and speed effect
    +custom source for environment reflection
    +3 color gradient for environment lighting
    +fixed pitch torque direction

--- SpaceBounty/Assets/Deepanshu/Scripts1/Player/PlayerController.cs
+++ SpaceBounty/Assets/Deepanshu/Scripts1/Player/PlayerController.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] [Range(1000f, 10000f)] //for scrollbar
+    [SerializeField] [Range(1000f, 10000f)]
     float _thrustForce = 7500,
           _pitchForce = 6000,
           _rollForce = 1000
@@ -13,17 +13,27 @@ public class PlayerController : MonoBehaviour
                  ,_pitchAmount
                  ,_rollAmount
                  ,_yawAmount;
-
     private void Awake()
     {
       _rigidbody = GetComponent<Rigidbody>();
     }
-
     private void FixedUpdate()
     {
         if (!Mathf.Approximately(0f, _pitchAmount))
         {
-           _rigidbody.AddTorque(transform.right *(_pitchForce * _pitchAmount * Time.fixedDeltaTime)); //torque in horizontal axis 
+           _rigidbody.AddTorque(transform.forward *(_pitchForce * _pitchAmount * Time.fixedDeltaTime));
+        }
+        if (!Mathf.Approximately(0f, _rollAmount))
+        {
+            _rigidbody.AddTorque(transform.right *(_rollForce * _rollAmount * Time.fixedDeltaTime));
+        }
+        if (!Mathf.Approximately(0f, _yawAmount))
+        {
+            _rigidbody.AddTorque(transform.up * (_YawForce * _yawAmount * Time.fixedDeltaTime)); 
+        }
+        if (!Mathf.Approximately(0f, _thrustAmount))
+        {
+            _rigidbody.AddForce(transform.right * (_thrustForce * _thrustAmount * Time.fixedDeltaTime)); 
         }
     }
 }   
